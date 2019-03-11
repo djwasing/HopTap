7e7ba3cc834298eb78fe2fbc54ca22d71ba82127
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-03-10 21:06:01 -0500

    seed/schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
-create database whiskey_db;
+drop database if exists whiskey_db;
 use whiskey_db;
 
 CREATE TABLE whiskeyTable (
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR (100) NULL,
-sampled tinyint(1),
+sampled BOOLEAN DEFAULT false,
 PRIMARY KEY (id) 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,4 @@
-insert into whiskeyTable (name, sampled) values ("Jack Daniels", "false");
-insert into whiskeyTable (name, sampled) values ("Jim Beam", "false");
-insert into whiskeyTable (name, sampled) values ("Bird Dog", "false");
-insert into whiskeyTable (name, sampled) values ("Crown Royal", "false");
-insert into whiskeyTable (name, sampled) values ("Crown Royal: Regal Apple", "false");
-insert into whiskeyTable (name, sampled) values ("Canadian Mist", "false");
-insert into whiskeyTable (name, sampled) values ("Johnnie Walker", "false");
+insert into whiskeyTable (name) values ("Jack Daniels");
+insert into whiskeyTable (name) values ("Jim Beam");
+insert into whiskeyTable (name) values ("Bird Dog");
+insert into whiskeyTable (name) values ("Crown Royal");
