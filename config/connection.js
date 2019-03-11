44d2a3c7374ad3f2076c8fbe410c5cb5ec7b4afc
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-03-10 21:59:26 -0500

    ku

--- config/connection.js
+++ config/connection.js
@@ -1,41 +1,26 @@
-
 var mysql = require("mysql");
+var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Riften#333',
-    database: 'whiskey_db'
-  });
-};
+if(process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-// Setting up our connection information
-var source = {
-  localhost: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Riften#333",
-    database: "whiskey_db"
-  }
-};
-
-
-// Creating our connection
-var connection = mysql.createConnection(source.localhost);
-
-
-// Connecting to the database.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+} else{
+ connection = mysql.createConnection({
+ host: "localhost",
+ port: 3306,
+ user: "root",
+ password: "Riften#333",
+ database: "whiskey_db"
 });
 
-// Exporting our connection
+}
+
+connection.connect(function(err) {
+ if (err) {
+   console.error("error connecting: " + err.stack);
+   return;
+ }
+ console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
